70d25e01aa75b9e020de5c4ba0751107305b5899
Aleem Ur-Rehman <aleem00gpu@outlook.com>
2019-02-24 21:11:47 +0000

    sign in ui cleaned up

--- disso-code/src/components/auth/signIn.js
+++ disso-code/src/components/auth/signIn.js
@@ -23,102 +23,57 @@ class SignIn extends Component {
     const { email, password} = this.state;
     if(auth.uid) return <Redirect to='/dashboard'/>
     return (
-      // <div class="container">
-      //   <Segment inverted>
-      //   <Form inverted>
-      //     <Form.Group widths='seven'>
-      //       <Form.Input 
-      //         placeholder='email' 
-      //         name='email'
-      //         type='email'
-      //         value={email}
-      //         onChange={this.handleChange}
-      //         />
-      //       <Form.Input 
-      //         placeholder='password'
-      //         type='password' 
-      //         name='password'
-      //         value={password}
-      //         onChange={this.handleChange}
-      //         />
-      //       <Form.Button type="submit"
-      //       onClick={this.handleSubmit}>
-      //       Login</Form.Button>
-      //     </Form.Group>
-      //   </Form>
-      //   </Segment>
-      // </div>
+      <div className='login-form'>
+      <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.login-form {
+          height: 100%;
+        }
+      `}</style>
+      <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Form size='large' inverted>
+            <Segment inverted stacked>
+              <Form.Input fluid icon='user' 
+              iconPosition='left' 
+              placeholder='email' 
+              name='email'
+              type='email'
+              value={email}
+              onChange={this.handleChange} 
+              />
+              <Form.Input
+                fluid
+                icon='lock'
+                iconPosition='left'
+                placeholder='password'
+                type='password' 
+                name='password'
+                value={password}
+                onChange={this.handleChange}
+              />
 
-<div className='login-form'>
-<style>{`
-  body > div,
-  body > div > div,
-  body > div > div > div.login-form {
-    height: 100%;
-  }
-`}</style>
-<Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-  <Grid.Column style={{ maxWidth: 450 }}>
-    <Form size='large' inverted>
-      <Segment inverted stacked>
-        <Form.Input fluid icon='user' 
-        iconPosition='left' 
-        placeholder='email' 
-        name='email'
-        type='email'
-        value={email}
-        onChange={this.handleChange} 
-        />
-        <Form.Input
-          fluid
-          icon='lock'
-          iconPosition='left'
-          placeholder='password'
-          type='password' 
-          name='password'
-          value={password}
-          onChange={this.handleChange}
-        />
-
-        <Form.Button 
-        color='teal' 
-        fluid size='small'
-        onClick={this.handleSubmit}>
-          Login
-        </Form.Button>
-      </Segment>
-    </Form>
-    { authError ? 
-    <Message
-      attached='bottom' warning>
-      { this.props.authError ? <p>{this.props.authError}</p> : null }
-    </Message> 
-    : null }
-    <Message>
-      New User? <a href='/signup'>Sign Up</a>
-    </Message>
-  </Grid.Column>
-</Grid>
-</div>
-      
-      // <div className="container">
-      //   <form className="white" onSubmit={this.handleSubmit}>
-      //     <h5 className="grey-text text-darken-3">Sign In</h5>
-      //     <div className="input-field">
-      //       <label htmlFor="email">Email</label>
-      //       <input type="email" id='email' onChange={this.handleChange} />
-      //     </div>
-      //     <div className="input-field">
-      //       <label htmlFor="password">Password</label>
-      //       <input type="password" id='password' onChange={this.handleChange} />
-      //     </div>
-      //     <div className="input-field">
-      //       <div className="center red-text">
-      //         { authError ? <p>{authError}</p> : null }
-      //       </div>
-      //     </div>
-      //   </form>
-      // </div>
+              <Form.Button 
+              color='teal' 
+              fluid size='small'
+              onClick={this.handleSubmit}>
+                Login
+              </Form.Button>
+            </Segment>
+          </Form>
+          { authError ? 
+          <Message
+            attached='bottom' warning>
+            { this.props.authError ? <p>{this.props.authError}</p> : null }
+          </Message> 
+          : null }
+          <Message>
+            New User? <a href='/signup'>Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+      </div>
     )
   }
 }
